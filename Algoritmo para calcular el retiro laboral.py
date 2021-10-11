5df620a488c42a6b074bd75a5b9f5afaa6a45d8f
D13G0S <90343875+D13G0S@users.noreply.github.com>
2021-10-11 07:48:50 -0500

    Create Algoritmo para calcular el retiro laboral.py

---
+++ Algoritmo para calcular el retiro laboral.py
@@ -0,0 +1,12 @@
+Algoritmo para calcualr año de retiro
+
+1. Inicio
+2. print "cual es su edad"
+3. Hacer edad = entrada del usuario 
+4. print "A que edad desea retirarse"
+5. Hacer edad_de_retiro = entrada del usuario
+6. Hacer año = 2021
+7. hacer año_para_retiro = edad_de_retiro - edad
+8. hacer año_de_retiro = año_actual + año_de_reiro
+9. Imprimir "es el año", año_actual, ", por lo tanto, te retiraras dentro de ", años_para_retiro, "en el", año_de_retiro 
+10. FIn
