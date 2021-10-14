fb3182ba0dd3db3c987d3ccb54483f1fb4adbec2
D13G0S <90343875+D13G0S@users.noreply.github.com>
2021-10-14 22:34:15 +0000

    Cambios para el programa en Python

---
+++ edad_de_retiro.py
@@ -0,0 +1,24 @@
+# este trabajo calcula el año en el que una persona puede retirarse del trabajo
+# 13 de octubre de 2021
+# Autor: Diego Hernández Sánchez
+
+#1. Inicio
+#2. Definir año_actual = 2021
+año_actual = 2021
+#3. Imprimir "Cual es tu edad?"
+print("Cual es tu edad")
+#4. Hacer edad_actual = entrada de usuario
+edad_actual = int (input())
+#5. Imprimir "a que edad deseas retirarte"
+print ("A que edad deseas retirarse")
+#6. Hacer edad_retiro = entrada del usuario
+edad_de_retiro int(input())
+#7. Hacer años_para_retiro = edad_retiro - edad_actual
+años_para_retiro = edad_de_retiro - edad_actual
+#8. Hacer años_para_retiro = año_actual + años_para_retiro
+años_para_retiro = año_actual + años_para_retiro
+#9. Imprimir "Tienes "años_para_retiro" años antes de que te puedas retirar"
+print ("Tienes "años_para_retiro," años antes de que te puedas retirar.")
+#10. Imprimir "Lo podras hacer en el año " años_para_retiro
+print("Lo podrás hacer en el año",años_para_retiro)
+#=11. FIN
