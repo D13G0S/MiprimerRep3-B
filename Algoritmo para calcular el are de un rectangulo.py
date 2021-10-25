05fcb24233dd7eae93ea91dabc8dfb8ed318cf40
D13G0S <90343875+D13G0S@users.noreply.github.com>
2021-10-25 17:05:31 -0500

    Create Algoritmo para calcular el are de un rectangulo.py

---
+++ Algoritmo para calcular el are de un rectangulo.py
@@ -0,0 +1,8 @@
+imprimir: "¿Cual es el largo del cuarto en metros?"
+ Imprimir: 4
+  Imprimir: "Cual es el ancho del cuarto en metros?"
+    Imprimir: 3
+      Imprimir: Ingresaste los las siguientes dimensiones: 3 x 4 metros 
+        Imprimir: El area es de: 
+          Imprimir: 12 metros cuadrados
+            Imprimir: 39.37008 pies cuadrados
